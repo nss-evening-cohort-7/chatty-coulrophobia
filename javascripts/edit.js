f3fe5154f1f7e32787933922fc9342887f7c8aaa
Xuehui Chen <jeffreychen2016@gmail.com>
2018-04-24 20:24:45 -0500

    save works

--- javascripts/edit.js
+++ javascripts/edit.js
@@ -1,7 +1,23 @@
 const getCurrentTime = require('./timestamp');
 
+const focusInputField = (chatEntry) => {
+  chatEntry.focus();
+};
+
+const highlightMessageBeingEdited = (e) => {
+  const individualMessageBox = e.target.parentNode.parentNode.parentNode.parentNode;
+  individualMessageBox.classList.add('highlight');
+};
+
+const removeHighlight = () => {
+  const highlightedMessages = document.getElementsByClassName('highlight');
+  for (let i = 0; i < highlightedMessages.length; i++) {
+    highlightedMessages[i].classList.remove('highlight');
+  };
+};
+
 const retrieveMessage = (e) => {
-  if (e.target.classList[2] === 'btn-message-edit') {
+  if (e.target.classList.contains('btn-message-edit')) {
     const messageLine = e.target.parentNode.parentNode.parentNode.children[0].children[1].children[0].children[0];
     const lastEditedLine = e.target.parentNode.parentNode.parentNode.children[0].children[0].children[2];
     const originalMessage = messageLine.innerHTML;
@@ -9,8 +25,9 @@ const retrieveMessage = (e) => {
     messageLine.parentNode.id = 'message-back';
     lastEditedLine.id = 'last-edited-back';
     const chatEntry = document.getElementById('chat-entry');
-
     chatEntry.value = originalMessage;
+    focusInputField(chatEntry);
+    highlightMessageBeingEdited(e);
   };
 };
 
@@ -27,6 +44,8 @@ const reprintMessage = () => {
     document.getElementById('chat-entry').value = '';
     message.id = '';
     editedTime.id = '';
+
+    removeHighlight();
   };
 };
 
