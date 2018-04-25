c95080176456735e83a6522fede3688c31a41372
Andy <amillion3@gmail.com>
2018-04-24 22:30:23 -0500

    WIP

--- javascripts/delete.js
+++ javascripts/delete.js
@@ -3,8 +3,8 @@ const messages = require('./messages');
 const originalMessages = messages.getMessages();
 
 const deleteThisMessage = (e) => {
-  console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.id);
-  const messageSelectedId = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+  console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id);
+  const messageSelectedId = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id;
   originalMessages.forEach((message) => {
     if (message.messageId === messageSelectedId) {
       console.log('message.messageId', message.messageId);
