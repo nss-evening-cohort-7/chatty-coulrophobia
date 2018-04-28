aa3851abb43a87ec35329b668fc5e8b9eea75da0
Amanda Slayton <alslayton86@gmail.com>
2018-04-28 13:34:04 -0500

    did things

--- javascripts/chatBot.js
+++ javascripts/chatBot.js
@@ -1,14 +1,16 @@
 const messages = require('./messages');
 const buildString = require('./domStringBuilder');
 
+let getLastMessage = '';
+
 const startReply = () => {
   const timeAmt = Math.floor(Math.random() * 10000) + 1;
+  getLastMessage = messages.getMessages()[messages.getMessages().length - 1].message;
   setTimeout(writeReply, timeAmt);
 };
 
 const writeReply = () => {
   const chatBotUsers = messages.getChatBots();
-  const getLastMessage = messages.getMessages()[messages.getMessages().length - 1].message;
   const randomChatbot = Math.floor(Math.random() * Math.floor(chatBotUsers.length));
   const keywords = chatBotUsers[randomChatbot].response.keywords;
 
