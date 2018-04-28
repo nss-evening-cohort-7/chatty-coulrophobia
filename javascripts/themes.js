0225a412de614a02eb3e0be87d38bdac0107a9cf
Xuehui Chen <jeffreychen2016@gmail.com>
2018-04-28 08:48:51 -0500

    added light and dark theme

--- javascripts/themes.js
+++ javascripts/themes.js
@@ -10,25 +10,28 @@ const applyTheme = (e) => {
   const backgroundChangeBtn = document.getElementById('chng-bkgrd-color').innerHTML;
   const textChangeBtn = document.getElementById('chng-txt-color').innerHTML;
   const messageContainers = document.getElementsByClassName('message');
+  const darkBtn = document.getElementById('dark-theme');
+  const lightBtn = document.getElementById('light-theme');
+  const customBtn = document.getElementById('custom-theme');
   if (e.target.id === 'dark-theme') {
-    document.getElementById('custom-theme').classList.remove('active');
-    document.getElementById('light-theme').classList.remove('active');
+    customBtn.classList.remove('active');
+    lightBtn.classList.remove('active');
     e.target.classList.add('active');
     for (let i = 0; i < messageContainers.length; i++) {
       messageContainers[i].style.backgroundColor = `DarkGray`;
       messageContainers[i].style.color = `White`;
     };
   } else if (e.target.id === 'light-theme') {
-    document.getElementById('custom-theme').classList.remove('active');
-    document.getElementById('dark-theme').classList.remove('active');
+    customBtn.classList.remove('active');
+    darkBtn.classList.remove('active');
     e.target.classList.add('active');
     for (let i = 0; i < messageContainers.length; i++) {
       messageContainers[i].style.backgroundColor = `White`;
       messageContainers[i].style.color = `Black`;
     };
   } else {
-    document.getElementById('light-theme').classList.remove('active');
-    document.getElementById('dark-theme').classList.remove('active');
+    lightBtn.classList.remove('active');
+    darkBtn.classList.remove('active');
     e.target.classList.add('active');
     for (let i = 0; i < messageContainers.length; i++) {
       messageContainers[i].style.backgroundColor = `#${backgroundChangeBtn}`;
